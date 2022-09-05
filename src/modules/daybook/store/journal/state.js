b9e6362f3867817392ec7adbc1e30118739f89fc
hector.frasson <hector.frasson@lemondata.com.ar>
2022-09-05 16:45:16 -0300

    Se cambia el formato de las entries. Journal State.

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,5 +1,24 @@
 //El state es reactivo.
 export default () => ({
     isLoading: true,
-    entries:[1,2,3,4,5,6]
+    entries:[
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Hic asperiores molestias delectus, perspiciatis veniam saepe, praesentium quam itaque unde, vero nesciunt! Accusamus iste iusto deleniti rerum provident officiis ea beatae!',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Molestias delectus, perspiciatis veniam saepe, praesentium quam itaque unde, vero nesciunt! Accusamus iste iusto deleniti rerum provident officiis ea beatae!',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Perspiciatis veniam saepe, praesentium quam itaque unde, vero nesciunt! Accusamus iste iusto deleniti rerum provident officiis ea beatae!',
+            picture: null
+        }
+    ]
 })
