40dbe64bfbb7d91272db240c9090d84221cc2b82
hector.frasson <hector.frasson@lemondata.com.ar>
2022-09-05 18:30:45 -0300

    Componente Entry: Informacion al componente.

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -9,7 +9,10 @@
         />
     </div>
     <div class="entry-scrollarea">
-        <Entry v-for="item in entriesByTerm" :key="item" />
+        <Entry v-for="entry in entriesByTerm" 
+               :key="entry.id" 
+               :entry="entry" 
+        />
     </div>
   </div>
 </template>
