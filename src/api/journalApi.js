f7fcd921be0802490894c4345f17dc0d43beef7f
hector.frasson <hector.frasson@lemondata.com.ar>
2022-10-21 19:24:23 -0300

    Interceptors: Cada vez que el token se actualiza toma la ultima version del mismo.

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -5,4 +5,13 @@ const journalApi = axios.create({
     baseURL:'https://vue---demos-default-rtdb.firebaseio.com'
 })
 
+//Cada vez que el token se actualiza toma la ultima version del mismo
+journalApi.interceptors.request.use( (config) => {
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+
+    return config
+})
+
 export default journalApi
