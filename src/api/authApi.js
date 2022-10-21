f92fe92ae88de02a135e85af201cf822a90e7908
frass-stack <60819298+frass-stack@users.noreply.github.com>
2022-10-21 18:01:39 -0300

    Update authApi.js

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const authApi = axios.create({
     baseURL:'https://identitytoolkit.googleapis.com/v1/accounts',
     params:{
-        key:'AIzaSyBJC4dZ0OixNeWbAYugZqGUpcCfkxH0vUM'
+        key:'Aca va una api key'
     }
 })
 
